d26d78b49a7f5ad2496593d1bae5126869f06edf
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2021-04-14 19:48:02 +0000

    foxy branch

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,19 +1,39 @@
-#include "ros/ros.h"
-#include "sensor_msgs/LaserScan.h"
-#include "std_msgs/Float32.h"
+#include <memory>
+#include <chrono>
+#include <cinttypes>
 
-ros::Publisher pub_left;
-ros::Publisher pub_right;
+//add the ROS header
+#include "rclcpp/rclcpp.hpp"
+//add the headers for 
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/float32.hpp"
 
-void sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+
+using std::placeholders::_1;
+
+class MyController : public rclcpp::Node
 {
+public:
+  MyController()
+  : Node("my_controller")
+  { 
+    publisher_left_ = this->create_publisher<std_msgs::msg::Float32>("/leftwheel_vel", 1);
+    publisher_right_ = this->create_publisher<std_msgs::msg::Float32>("/rightwheel_vel", 1);
+    subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "/proxy_sensor", 10, std::bind(&MyController::sensor_callback, this, _1));
+  }
+      
+private:
 
-	std_msgs::Float32 vel_l;
-	std_msgs::Float32 vel_r;
-	bool obstacle=false;
-	bool ob_left=false;
-	bool ob_front=false;
-	bool ob_right=false;
+  void sensor_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+  {
+    auto vel_l = std_msgs::msg::Float32();
+    auto vel_r = std_msgs::msg::Float32();
+    
+    bool obstacle=false;
+    bool ob_left=false;
+    bool ob_front=false;
+    bool ob_right=false;
 	for(int i=0;i<8;i++){
 		if (msg->intensities[i]>0.0){
 			if(i<3)
@@ -51,24 +71,20 @@ void sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 		vel_l.data=1.0;
 		vel_r.data=0.0;
 		}
-	
-	
 	}
-	pub_left.publish(vel_l);
-	pub_right.publish(vel_r);
-}
+	publisher_left_->publish(vel_l);
+	publisher_right_->publish(vel_r);
+  }
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_left_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_right_;
+};
 
-int main(int argc, char **argv)
+int main(int argc, char * argv[])
 {
-   ros::init(argc, argv, "turtlebot_controller");
-   ros::NodeHandle n;
-   
-   pub_left = n.advertise<std_msgs::Float32>("/leftwheel_vel", 1000);
-   pub_right = n.advertise<std_msgs::Float32>("/rightwheel_vel", 1000);
-   
-   ros::Subscriber sub = n.subscribe("/proxy_sensor", 1000, sensorCallback);
-
-   ros::spin();
-
-   return 0;
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MyController>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
